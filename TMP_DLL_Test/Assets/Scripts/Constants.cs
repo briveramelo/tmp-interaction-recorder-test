c86475647f45ac7038577dfd79d6ebb0cd6d1edc
Brandon Rivera-Melo <briveramelo@gmail.com>
2019-11-26 12:04:48 -0600

    api not updated ui contents

---
+++ TMP_DLL_Test/Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+namespace BRM.InteractionRecorder.UnityUi
+{
+    public static class Constants
+    {
+        public const string PackageDeveloper = "BRM";
+        public const string DisplayedAppName = "Interaction Recorder";
+        public const string FileAppName = "InteractionRecorder";
+    }
+}
