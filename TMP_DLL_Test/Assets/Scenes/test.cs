5e70e86ffb155627357d136ca1e3873193ee0a3f
Brandon Rivera-Melo <briveramelo@gmail.com>
2019-11-26 12:00:15 -0600

    required content

---
+++ TMP_DLL_Test/Assets/Scenes/test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class test : Button
+    {
+        void Start()
+        {
+            Input.GetButton("");
+        }
+    }
+}
